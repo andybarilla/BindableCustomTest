4255585d636997ed7c696a76a3693d4ee1ebced9
Andrew Barilla <andy@Andrews-MacBook-Pro.local>
2015-03-31 17:50:04 -0600

    First commit

---
+++ TestMVVM/CustomEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TestMVVM
+{
+	public class CustomEntry : StackLayout
+	{
+		protected Entry entry;
+
+		public CustomEntry ()
+		{
+			entry = new Entry ();
+
+			Children.Add (entry);
+		}
+
+		public static readonly BindableProperty TextProperty=BindableProperty.Create<CustomEntry, string>( p => p.Text, "" );
+
+		public String Text {
+			get { return entry.Text; }
+			set { entry.Text = value; }
+		}
+	}
+}
+
---
+++ TestMVVM/TestViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace TestMVVM
+{
+	public class TestViewModel : INotifyPropertyChanged
+	{
+		double testValue;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public TestViewModel ()
+		{
+			var random = new Random ();
+			testValue = random.NextDouble ();
+		}
+
+		public double TestValue 
+		{
+			set {
+				if (testValue != value) {
+					testValue = value;
+					OnPropertyChanged ("TestValue");
+				}
+			}
+			get {
+				return testValue;
+			}
+		}
+
+		protected virtual void OnPropertyChanged (string propertyName) {
+			if (PropertyChanged != null) {
+				PropertyChanged (this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
+
